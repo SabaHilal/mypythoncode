3ed5946854d1110a8869eaa2eedb85a48026e952
SabaHilal <57343679+SabaHilal@users.noreply.github.com>
2023-09-08 15:09:36 +0530

    Add files via upload

---
+++ sincostan.py
@@ -0,0 +1,13 @@
+import math
+
+given_value = 0
+
+def sincostanfunc(x):
+    print("sin ","%.1f" %x , math.sin(math.pi/3))
+    print("cos ", "%.1f" %x, math.cos(math.pi/6))
+    print("tan ", "%.1f" % x, math.tan(math.pi/3))
+    
+while given_value <= 10:
+    print('given value is: ', "%.1f" % given_value)
+    sincostanfunc(given_value)
+    given_value = given_value + 0.2
